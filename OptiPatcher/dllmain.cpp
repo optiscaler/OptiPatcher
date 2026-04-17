3c4a858c364a56c33219627cefe261782037a870
TheRazerMD <derazer1950@gmail.com>
2026-04-17 02:17:09 +0200

    Fixed ROMEO DLSS pattern
    * ROMEO IS A DEAD MAN - for update 1.4

--- OptiPatcher/dllmain.cpp
+++ OptiPatcher/dllmain.cpp
@@ -578,8 +578,8 @@ static void CheckForPatch()
     // Edition, Manor Lords, Nobody Wants to Die, Valor Mortis playtest, Fort Solis, Spirit of the North 2, Tokyo Xtreme
     // Racer/Shutokou Battle, INDUSTRIA 2 Demo, REANIMAL (+ Demo), Keeper (+WinGDK PaganIdol exe), Stygian: Outer Gods,
     // Tormented Souls 2, Assetto Corsa Rally, SpongeBob SquarePants: Titans of the Tide, Echoes of the End: Enhanced
-    // Edition, Supraworld, ROMEO IS A DEAD MAN, Solasta II, Carmageddon: Rogue Shift, Half Sword, I Am Jesus Christ,
-    // Star Trek: Voyager - Across the Unknown
+    // Edition, Supraworld, Solasta II, Carmageddon: Rogue Shift, Half Sword, I Am Jesus Christ, Star Trek: Voyager -
+    // Across the Unknown
     else if (CHECK_UE(robocopunfinishedbusiness) || CHECK_UE(ninjagaiden2black) || CHECK_UE(hellisus) ||
              CHECK_UE(brothers) || CHECK_UE(otherskin) || CHECK_UE(thesinkingcityremastered) ||
              CHECK_UE(chernobylite2) || CHECK_UE(commandos) || CHECK_UE(mindseye) || CHECK_UE(crtoiprototype) ||
@@ -587,9 +587,8 @@ static void CheckForPatch()
              CHECK_UE(manorlords) || CHECK_UE(detnoir) || CHECK_UE(minotaur) || CHECK_UE(sycamore) || CHECK_UE(sotn2) ||
              CHECK_UE(tokyoxtremeracer) || CHECK_UE(industria_2) || exeName == "reanimal.exe" || CHECK_UE(keeper) ||
              CHECK_UE(paganidol) || CHECK_UE(stygian) || CHECK_UE(tormentedsouls2) || exeName == "acr.exe" ||
-             CHECK_UE(ghost) || CHECK_UE(thedarken) || CHECK_UE(supraworld) || CHECK_UE(sevgame) ||
-             CHECK_UE(brimstone) || CHECK_UE(carma) || CHECK_UE(halfswordue5) || CHECK_UE(imjch) ||
-             CHECK_UE(stvoyagersteam))
+             CHECK_UE(ghost) || CHECK_UE(thedarken) || CHECK_UE(supraworld) || CHECK_UE(brimstone) || CHECK_UE(carma) ||
+             CHECK_UE(halfswordue5) || CHECK_UE(imjch) || CHECK_UE(stvoyagersteam))
     {
         std::string_view pattern("84 C0 49 8B C7 74 03 49 8B C5 46 8B 34 30 E8 ? ? ? ? 84 C0 75");
         auto patchAddress = (void*) scanner::GetAddress(exeModule, pattern, 19);
@@ -1010,6 +1009,34 @@ static void CheckForPatch()
         {
             std::string_view pattern2("4C 89 B4 24 38 02 00 00 E8 ? ? ? ? 84 C0 75");
             auto patchAddress2 = (void*) scanner::GetAddress(exeModule, pattern2, 13);
+
+            if (patchAddress2 != nullptr)
+            {
+                std::vector<BYTE> patch = { 0x0C, 0x01 };
+                patcher::PatchAddress(patchAddress2, &patch);
+            }
+
+            _patchResult = patchAddress2 != nullptr;
+        }
+    }
+
+    // ROMEO IS A DEAD MAN
+    else if (CHECK_UE(sevgame))
+    {
+        std::string_view pattern("84 C0 49 8B C7 74 03 49 8B C5 46 8B 34 30 E8 ? ? ? ? 84 C0 75");
+        auto patchAddress = (void*) scanner::GetAddress(exeModule, pattern, 19);
+        if (patchAddress != nullptr)
+        {
+            std::vector<BYTE> patch = { 0x0C, 0x01 };
+            patcher::PatchAddress(patchAddress, &patch);
+            _patchResult = true;
+        }
+
+        // For 1.4.000 update
+        else
+        {
+            std::string_view pattern2("44 8B ? ? ? ? ? BA B8 16 00 00 84 C0 75");
+            auto patchAddress2 = (void*) scanner::GetAddress(exeModule, pattern2, 12);
 
             if (patchAddress2 != nullptr)
             {
@@ -1569,7 +1596,7 @@ static void CheckForPatch()
     }
 
     //// Crimson Desert
-    // else if (exeName == "crimsondesert.exe")
+    // else if (exeName == "NOT-crimsondesert.exe")
     //{
     //     // Emulate only the tiny post-helper state for the DLSS path.
     //     std::string patternCodeCave;
